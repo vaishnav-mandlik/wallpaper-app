afde35a9b110ef1204b2d413b9797dd160b13787
vaishanv-mandlik <vaishnavmandlik@yahoo.com>
2023-10-21 18:33:11 +0530

    add images

--- components/Home/Category.js
+++ components/Home/Category.js
@@ -1,6 +1,13 @@
-import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  Dimensions,
+} from "react-native";
 import React, { useState } from "react";
-import { FlatList, ScrollView } from "react-native-gesture-handler";
+import { FlatList } from "react-native-gesture-handler";
 import { CategoryList } from "../Constants";
 import useFetch from "../../hook/useFetch";
 
@@ -34,65 +41,54 @@ const CategoryListUi = ({ item, isActive, onPress }) => {
 };
 
 const Category = () => {
-  const [activeCategory, setActiveCategory] = useState(
-    CategoryList[0]?.label || "CategoryList[0].label"
-  );
-  const [page, setPage] = useState(1);
-  const { data, loading } = useFetch(
-    "search",
-    `${activeCategory} wallpaper`,
-    page
-  );
+  const [activeCategory, setActiveCategory] = useState(CategoryList[0]?.label);
+  const { data } = useFetch("search", `${activeCategory} wallpaper`, 1);
   console.log(data, "data from category.js");
 
   const handleCategoryPress = (categoryId) => {
     setActiveCategory(categoryId);
   };
-
-  const handleLoadMore = () => {
-    if (!loading) {
-      setPage((prevPage) => prevPage + 1);
-    }
-  };
+  const windowWidth = Dimensions.get("window").width;
+  const imageWidth = windowWidth / 3 - 20;
 
   return (
     <View style={styles.cardContainer}>
       <Text style={styles.heading}>Category</Text>
-
-      <FlatList
-        data={CategoryList}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        keyExtractor={(item) => item.label.toString()}
-        renderItem={({ item }) => (
-          <CategoryListUi
-            item={item}
-            isActive={item.label === activeCategory}
-            onPress={handleCategoryPress}
-          />
-        )}
-      />
-      <ScrollView>
-        <View>
-          {data && (
-            <FlatList
-              data={data.photos}
-              keyExtractor={(item) => item.id.toString()}
-              renderItem={({ item }) => (
-                <Image
-                  source={{ uri: item.src.medium }}
-                  style={{ width: 200, height: 200, margin: 5 }}
-                />
-              )}
-              onEndReached={handleLoadMore}
-              onEndReachedThreshold={0.5}
-              ListFooterComponent={() =>
-                loading ? <Text>Loading...</Text> : null
-              }
+      <View>
+        <FlatList
+          data={CategoryList}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item.label.toString()}
+          renderItem={({ item }) => (
+            <CategoryListUi
+              item={item}
+              isActive={item.label === activeCategory}
+              onPress={handleCategoryPress}
             />
           )}
-        </View>
-      </ScrollView>
+        />
+      </View>
+      <View>
+        {data && (
+          <FlatList
+            data={data.photos}
+            numColumns={3}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <Image
+                source={{ uri: item.src.medium }}
+                style={{
+                  width: imageWidth,
+                  height: imageWidth,
+                  margin: 10,
+                  borderRadius: 10,
+                }}
+              />
+            )}
+          />
+        )}
+      </View>
     </View>
   );
 };
